f3e3d6bc059f98766d17567ed234954f62053967
AriiMoose <ariimoose@gmail.com>
2015-04-21 15:22:39 +0100

    Assignment Submission

---
+++ Assets/Code/BGE/Lazer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using UnityEngine;
+
+namespace BGE
+{
+	class Lazer:MonoBehaviour
+	{
+	    public String lazerTargetTag;
+	    public GameObject lazerTarget;
+	    private LineRenderer line;
+	    private Vector3 targetPos;
+	    private ParticleSystem explosion;
+
+	    public void Start()
+	    {
+	        line = gameObject.GetComponent<LineRenderer>();
+	        line.enabled = false;
+	    }
+
+        public void Update()
+        {
+            if (lazerTarget == null && lazerTargetTag != null)
+            {
+                lazerTarget = GameObject.FindGameObjectWithTag("Turian");
+            }
+
+            targetPos = lazerTarget.transform.position - transform.position;
+
+            if (Physics.Raycast(transform.position, targetPos, 70.0f))
+            {
+                lazerTarget.GetComponent<ParticleSystem>().Play();
+
+                StopCoroutine("shoot");
+                StartCoroutine("shoot");
+                
+            }
+        }
+
+        IEnumerator shoot()
+	    {
+            line.SetPosition(0, transform.position);
+            line.SetPosition(1, targetPos);
+            line.enabled = true;
+
+            Destroy(lazerTarget);
+
+            yield return new WaitForSeconds(1);
+            line.enabled = false;
+	    }
+	}
+}
---
+++ Assets/Scipts/CameraMan.cs
@@ -0,0 +1,74 @@
+﻿using BGE;
+using UnityEngine;
+using System.Collections;
+
+public class CameraMan : MonoBehaviour
+{
+
+    private Camera sov_cam;
+    private Camera da_cam;
+    private Camera norm_cam;
+
+    private bool da_used;
+    private bool sov_used;
+    private bool norm_used;
+    private bool fired;
+	// Use this for initialization
+	void Start ()
+	{
+	    sov_cam = GameObject.Find("Sovereign").GetComponentInChildren<Camera>();
+	    da_cam = GameObject.Find("DestinyAscension").GetComponentInChildren<Camera>();
+	    norm_cam = GameObject.Find("Normandy").GetComponentInChildren<Camera>();
+
+	    sov_cam.enabled = true;
+
+	    sov_used = true;
+	    da_used = false;
+	    norm_used = false;
+	    fired = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    if ((sov_used == true) && (da_used == false))
+	    {
+	        StartCoroutine(to_da());
+            StopCoroutine(to_da());
+	    }
+
+	    if ((da_used == true && (norm_used == false)))
+	    {
+	        StartCoroutine(to_norm());
+            StopCoroutine(to_norm());
+	    }
+	}
+
+    IEnumerator to_da()
+    {
+        yield return new WaitForSeconds(20);
+
+        sov_cam.enabled = false;
+        da_cam.enabled = true;
+
+        da_used = true;
+    }
+
+    IEnumerator to_norm()
+    {
+        yield return new WaitForSeconds(22);
+
+        da_cam.enabled = false;
+        norm_cam.enabled = true;
+
+        norm_used = true;
+
+        if (fired == false)
+        {
+            yield return new WaitForSeconds(5);
+            GameObject.Find("Normandy").GetComponentInChildren<Lazer>().StartCoroutine("shoot");
+            fired = true;
+        }
+        
+    }
+}
